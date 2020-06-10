c14f7d55caf5988a0f970964bb00b77da2f9e3c5
tarnaldo88 <tarnaldo88@gmail.com>
2020-06-10 12:16:43 -0700

    Added exercise page Update to exercise select added various images for front end

--- app/src/main/java/com/example/properform/Exercises.java
+++ app/src/main/java/com/example/properform/Exercises.java
@@ -2,13 +2,34 @@ package com.example.properform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Exercises extends AppCompatActivity {
+
+    private ImageButton BtnMoveBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercises);
+
+        BtnMoveBack = findViewById(R.id.backMainActivityBtn);
+
+
+
+        BtnMoveBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                moveToMainActivity();
+            }
+        });
+    }
+
+    private void moveToMainActivity(){
+        Intent goToMainActivity = new Intent(Exercises.this, ExerciseSelect.class);
+        startActivity(goToMainActivity);;
     }
 }
--- app/src/main/java/com/example/properform/RoutineSelect.java
+++ app/src/main/java/com/example/properform/RoutineSelect.java
@@ -2,13 +2,33 @@ package com.example.properform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class RoutineSelect extends AppCompatActivity {
+
+    private ImageButton BtnMoveBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routine_select);
+
+        BtnMoveBack = findViewById(R.id.backMainActivityBtn);
+
+
+        BtnMoveBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                moveToMainActivity();
+            }
+        });
+    }
+
+    private void moveToMainActivity(){
+        Intent goToMainActivity = new Intent(RoutineSelect.this, MainActivity.class);
+        startActivity(goToMainActivity);;
     }
 }
